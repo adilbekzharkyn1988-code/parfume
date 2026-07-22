3b137e07297b4d97cbf9f4dd6d9608333a5b5141
adilbekzharkyn1988-code <adilbek.zharkyn1988@gmail.com>
2026-07-22 12:05:47 +0500

    Update env.ts

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,3 +1,3 @@
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-01-01";
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "placeholder";
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "dg5kyqf3";

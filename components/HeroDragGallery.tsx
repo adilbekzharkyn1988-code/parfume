5d5e13006e272784471148997bb2f95281cbc3d8
adilbekzharkyn1988-code <adilbek.zharkyn1988@gmail.com>
2026-07-29 18:18:04 +0500

    Update HeroDragGallery.tsx

--- components/HeroDragGallery.tsx
+++ components/HeroDragGallery.tsx
@@ -135,7 +135,7 @@ export default function HeroDragGallery() {
         transition={{ duration: 1.1, delay: 0.3, ease: [0.16, 1, 0.3, 1] }}
         className="absolute inset-x-0 bottom-[40px] top-[8%] flex justify-center items-end z-10"
       >
-        <div className="relative w-[210px] md:w-[300px]">
+        <div className="relative w-[210px] md:w-[350px]">
   <Image
     src={women}
     alt="Парфюмерия"

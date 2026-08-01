24498c328649eb731cee5f9420ff95dca64ea35d
adilbekzharkyn1988-code <adilbek.zharkyn1988@gmail.com>
2026-08-01 11:50:07 +0500

    Add files via upload

--- contentful/client.ts
+++ contentful/client.ts
@@ -1,9 +1,7 @@
 import { createClient } from "contentful";
 
-const spaceId = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID || "e8y2nngpr6yc";
-const accessToken =
-  process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN ||
-  "upNLkRBWF2F3B_I_LkpJsd09KxfJh7_N2G2Fp_awRxY";
+const spaceId = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID || "";
+const accessToken = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN || "";
 
 export const client = createClient({
   space: spaceId,

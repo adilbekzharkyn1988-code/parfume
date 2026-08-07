6ee06166548b945e65eacbd34177b9ef09c1f953
adilbekzharkyn1988-code <adilbek.zharkyn1988@gmail.com>
2026-08-08 01:21:31 +0500

    Delete contentful/client.ts

--- contentful/client.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "contentful";
-
-const spaceId = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID || "";
-const accessToken = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN || "";
-
-export const client = createClient({
-  space: spaceId,
-  accessToken: accessToken,
-});
